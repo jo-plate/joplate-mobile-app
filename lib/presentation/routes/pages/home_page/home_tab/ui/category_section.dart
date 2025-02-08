3f90dc6743012841b3c8f83561bfc97b23222a1f
anas <a.khudeirat@hotmail.com>
2025-02-08 23:50:03 +0300

    consistentize paddings

--- lib/presentation/routes/pages/home_page/home_tab/ui/category_section.dart
+++ lib/presentation/routes/pages/home_page/home_tab/ui/category_section.dart
@@ -7,7 +7,7 @@ class CategorySection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         children: [
           const Row(
